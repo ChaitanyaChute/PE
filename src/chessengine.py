859f0f31a4f6b779ce7bd5f86647785766196354
Chaitanya-chute <chaitanyachute11@gmail.com>
2026-06-29 17:30:10 +0530

    implemented makeMove func to update game state

--- src/chessengine.py
+++ src/chessengine.py
@@ -29,4 +29,56 @@ class GameState:
         self.castle_rights_log = [CastleRights(self.current_castling_rights.wks, self.current_castling_rights.bks,
                                                self.current_castling_rights.wqs, self.current_castling_rights.bqs)]
 
+    def makeMove(self, move):
+        """
+        Takes a Move as a parameter and executes it.
+        (this will not work for castling, pawn promotion and en-passant)
+        """
+        self.board[move.start_row][move.start_col] = "--"
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.move_log.append(move)  # log the move so we can undo it later
+        self.white_to_move = not self.white_to_move  # switch players
+        self.move_counter = self.move_counter + 1
+        # update king's location if moved
+        if move.piece_moved == "wK":
+            self.white_king_location = (move.end_row, move.end_col)
+        elif move.piece_moved == "bK":
+            self.black_king_location = (move.end_row, move.end_col)
+
+        # pawn promotion
+        if move.is_pawn_promotion:
+            # if not is_AI:
+            #    promoted_piece = input("Promote to Q, R, B, or N:") #take this to UI later
+            #    self.board[move.end_row][move.end_col] = move.piece_moved[0] + promoted_piece
+            # else:
+            self.board[move.end_row][move.end_col] = move.piece_moved[0] + "Q"
+
+        # enpassant move
+        if move.is_enpassant_move:
+            self.board[move.start_row][move.end_col] = "--"  # capturing the pawn
+
+        # update enpassant_possible variable
+        if move.piece_moved[1] == "p" and abs(move.start_row - move.end_row) == 2:  # only on 2 square pawn advance
+            self.enpassant_possible = ((move.start_row + move.end_row) // 2, move.start_col)
+        else:
+            self.enpassant_possible = ()
+
+        # castle move
+        if move.is_castle_move:
+            if move.end_col - move.start_col == 2:  # king-side castle move
+                self.board[move.end_row][move.end_col - 1] = self.board[move.end_row][
+                    move.end_col + 1]  # moves the rook to its new square
+                self.board[move.end_row][move.end_col + 1] = '--'  # erase old rook
+            else:  # queen-side castle move
+                self.board[move.end_row][move.end_col + 1] = self.board[move.end_row][
+                    move.end_col - 2]  # moves the rook to its new square
+                self.board[move.end_row][move.end_col - 2] = '--'  # erase old rook
+
+        self.enpassant_possible_log.append(self.enpassant_possible)
+
+        # update castling rights - whenever it is a rook or king move
+        self.updateCastleRights(move)
+        self.castle_rights_log.append(CastleRights(self.current_castling_rights.wks, self.current_castling_rights.bks,
+                                                   self.current_castling_rights.wqs, self.current_castling_rights.bqs))
+    
     
