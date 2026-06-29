27d95fd6adec43a72c554593c64168a56e8de4da
Chaitanya-chute <chaitanyachute11@gmail.com>
2026-06-29 17:53:04 +0530

    king moves added

--- src/chessengine.py
+++ src/chessengine.py
@@ -507,4 +507,31 @@ class GameState:
         self.getBishopMoves(row, col, moves)
         self.getRookMoves(row, col, moves)
 
+    def getKingMoves(self, row, col, moves):
+        """
+        Get all the king moves for the king located at row col and add the moves to the list.
+        """
+        row_moves = (-1, -1, -1, 0, 0, 1, 1, 1)
+        col_moves = (-1, 0, 1, -1, 1, -1, 0, 1)
+        ally_color = "w" if self.white_to_move else "b"
+        for i in range(8):
+            end_row = row + row_moves[i]
+            end_col = col + col_moves[i]
+            if 0 <= end_row <= 7 and 0 <= end_col <= 7:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:  # not an ally piece - empty or enemy
+                    # place king on end square and check for checks
+                    if ally_color == "w":
+                        self.white_king_location = (end_row, end_col)
+                    else:
+                        self.black_king_location = (end_row, end_col)
+                    in_check, pins, checks = self.checkForPinsAndChecks()
+                    if not in_check:
+                        moves.append(Move((row, col), (end_row, end_col), self.board))
+                    # place king back on original location
+                    if ally_color == "w":
+                        self.white_king_location = (row, col)
+                    else:
+                        self.black_king_location = (row, col)
+    
     
