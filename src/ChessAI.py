64462e25f9d32a5a88f9a44fedd4a5f20c5eb99e
Chaitanya-chute <chaitanyachute11@gmail.com>
2026-06-30 20:22:54 +0530

    implemented complete ai player

--- src/ChessAI.py
+++ src/ChessAI.py
@@ -109,3 +109,115 @@ def findMoveNegaMaxAlphaBeta(game_state, valid_moves, depth, alpha, beta, turn_m
     return max_score
 
 # Transposition Table
+transposition_table = {} #used to not re-evaluate position that is already evaluated
+
+def findMove(game_state, valid_moves, return_queue):
+    global next_move, transposition_table
+    next_move = None
+    for depth in range(1, DEPTH + 1):
+        findMoveNegaMaxAlphaBetaTT(game_state, valid_moves, depth, -CHECKMATE, CHECKMATE, 1 if game_state.white_to_move else -1)
+        transposition_table = {}
+        if next_move is not None:
+            break
+    return_queue.put(next_move)
+
+
+def findMoveNegaMaxAlphaBetaTT(game_state, valid_moves, depth, alpha, beta, turn_multiplier):
+    global next_move, transposition_table
+    hash_key = game_state.getHashKey()
+    if hash_key in transposition_table:
+        return transposition_table[hash_key]
+
+    if depth == 0 or game_state.stalemate or game_state.checkmate:
+        return turn_multiplier * scoreBoard(game_state)
+
+    max_score = -CHECKMATE
+    ordered_moves = orderMoves(valid_moves, game_state)
+    for move in ordered_moves:
+        game_state.makeMove(move)
+        next_moves = game_state.getValidMoves()
+        score = -findMoveNegaMaxAlphaBetaTT(game_state, next_moves, depth - 1, -beta, -alpha, -turn_multiplier)
+        game_state.undoMove()
+        if score > max_score:
+            max_score = score
+            if depth == DEPTH:
+                next_move = move
+        alpha = max(alpha, score)
+        if alpha >= beta:
+            break
+
+    transposition_table[hash_key] = max_score
+    return max_score
+
+def controlCenterMoves(game_state, valid_moves):
+    """
+    Prioritize moves that control the center.
+    """
+    center_moves = []
+    for move in valid_moves:
+        if move.start_row in [1, 6] and abs(move.start_col - move.end_col) <= 1:
+            center_moves.append(move)
+        elif move.start_col == 4 or move.end_col == 4:
+            center_moves.append(move)
+    return center_moves
+
+def openingMoves(game_state, valid_moves):
+    """
+    Integrate famous opening moves.
+    """
+    opening_moves = {"Italian Game": ["e2e4", "e7e5", "g1f3", "b8c6", "f1c4"],
+                     "Sicilian Defense": ["e2e4", "c7c5"]}
+
+    current_moves = [move.getChessNotation() for move in game_state.move_log]
+    for opening in opening_moves.values():
+        if current_moves[:len(opening)] == opening:
+            return valid_moves[len(opening):] 
+
+    return valid_moves
+
+def findBestMove(game_state, valid_moves, return_queue):
+    global next_move
+    next_move = None
+    #random.shuffle(valid_moves)
+    findMoveNegaMaxAlphaBeta(game_state, valid_moves, DEPTH, -CHECKMATE, CHECKMATE,
+                             1 if game_state.white_to_move else -1)
+    return_queue.put(next_move)
+        
+def scoreBoard(game_state):
+    """
+    Score the board. A positive score is good for white, a negative score is good for black.
+    """
+    if game_state.checkmate:
+        if game_state.white_to_move:
+            return -CHECKMATE  # black wins
+        else:
+            return CHECKMATE  # white wins
+    elif game_state.stalemate:
+        return STALEMATE
+    score = 0
+    for row in range(len(game_state.board)):
+        for col in range(len(game_state.board[row])):
+            piece = game_state.board[row][col]
+            if piece != "--":
+                piece_position_score = 0
+                if piece[1] != "K":
+                    piece_position_score = piece_position_scores[piece][row][col]
+                if piece[0] == "w":
+                    score += piece_score[piece[1]] + piece_position_score
+                if piece[0] == "b":
+                    score -= piece_score[piece[1]] + piece_position_score
+
+    return score
+
+
+def findRandomMove(valid_moves):
+    """
+    Picks and returns a random valid move.
+    """
+    return random.choice(valid_moves)
+
+
+
+
+
+
